6ae13934a82409f8eeff8fab369b3bb453391bc5
Nana Duah <gabano2005@gmail.com>
2022-03-24 23:05:03 -0400

    Added script for local deploy

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-if [[ "$1" == "down" ]]; then
-    echo "Destroying..."
-    terraform -chdir=deployment destroy --auto-approve
-    exit 0
-fi
-
 
 BUILD_DIR=./.build
+LAMBDAFY="$BUILD_DIR/lambdafy"
 CURRENT_DIR=$(pwd)
 PYTHON_RUNTIME=python3.8
 VENV=$BUILD_DIR/venv
@@ -15,8 +10,18 @@ VENV=$BUILD_DIR/venv
 python3 -m venv $VENV
 
 
-mkdir $BUILD_DIR
-mkdir $BUILD_DIR/app_pkg
+if [[ "$1" == "down" ]]; then
+    echo "Destroying..."
+    terraform -chdir=$LAMBDAFY destroy --auto-approve
+    exit 0
+fi
+
+if [ ! -d "$LAMBDAFY" ]; then
+   git clone https://github.com/nanaduah1/infra4  $BUILD_DIR/infra4 \
+        && mkdir $BUILD_DIR/app_pkg \
+        && cp -r  $BUILD_DIR/infra4/lambdafy $BUILD_DIR \
+        && rm -fr $BUILD_DIR/infra4
+fi
 
 # Install dependencies
 $VENV/bin/pip install --upgrade pip \
@@ -24,8 +29,10 @@ $VENV/bin/pip install --upgrade pip \
     && cp -r $VENV/lib/$PYTHON_RUNTIME/site-packages/ $BUILD_DIR/app_pkg \
     && cp -r ./src/ $BUILD_DIR/app_pkg/ \
     && rm -fr $BUILD_DIR/venv \
-    && terraform -chdir=deployment init \
-    && terraform -chdir=deployment apply --auto-approve \
-    && rm -fr $BUILD_DIR \
+    && export TF_VAR_build_dir=$(realpath $BUILD_DIR) \
+    && terraform -chdir=$LAMBDAFY init \
+    && terraform -chdir=$LAMBDAFY apply --auto-approve \
+    && rm -fr $BUILD_DIR/app_pkg/* \
+    && rm $BUILD_DIR/app_pkg.zip \
     && echo "Deployment completed!!!"
 
